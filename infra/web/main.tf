85c0303dabb08dfb0c1545349c68470836e11670
Zakaria Mansouri <zakaria.mansouri@avimedical.com>
2023-04-16 04:05:21 +0200

    ./web deployment to aws using terraform

---
+++ infra/web/main.tf
@@ -0,0 +1,61 @@
+locals {
+  stage      = terraform.workspace
+  bucketName = "${local.stage}.algeriastartupjobs.com"
+  domainName = "${local.stage == "production" ? "www" : local.stage}.algeriastartupjobs.com"
+}
+
+provider "aws" {
+  region = "eu-west-1"
+}
+
+resource "aws_s3_bucket" "website" {
+  bucket = local.bucketName
+}
+
+resource "aws_s3_bucket_website_configuration" "website" {
+  bucket = aws_s3_bucket.website.bucket
+  index_document {
+    suffix = "index.html"
+  }
+  error_document {
+    key = "index.html"
+  }
+}
+
+resource "aws_s3_bucket_policy" "website" {
+  bucket = aws_s3_bucket.website.id
+  policy = jsonencode({
+    Version = "2012-10-17"
+    Statement = [
+      {
+        "Sid" : "PublicReadGetObject",
+        "Effect" : "Allow",
+        "Principal" : "*",
+        "Action" : "s3:GetObject",
+        "Resource" : "arn:aws:s3:::${local.bucketName}/*"
+      }
+    ]
+  })
+}
+
+# https://stackoverflow.com/a/57457344
+resource "null_resource" "remove_and_upload_website_to_s3" {
+  provisioner "local-exec" {
+    command = "aws s3 sync ${path.module}/../../web/dist s3://${aws_s3_bucket.website.id}"
+  }
+}
+
+resource "aws_route53_zone" "website" {
+  name = local.domainName
+}
+
+resource "aws_route53_record" "website-a" {
+  zone_id = aws_route53_zone.website.zone_id
+  name    = local.domainName
+  type    = "A"
+  alias {
+    name                   = aws_s3_bucket.website.website_endpoint
+    zone_id                = aws_s3_bucket.website.hosted_zone_id
+    evaluate_target_health = true
+  }
+}
