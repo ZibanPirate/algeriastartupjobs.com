2c9b9f7640732b5516ab01c02f4900c7958c1562
Zakaria Mansouri <zakaria.mansouri@avimedical.com>
2023-04-17 17:01:52 +0200

    cleaned up Vite template

---
+++ web/src/_entry/app.tsx
@@ -0,0 +1,3 @@
+export function App() {
+  return <div>JOIN A STARTUP IN ALGERIA</div>;
+}
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,10 +1,9 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "src/_entry/app";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
+createRoot(document.getElementById("root") as HTMLElement).render(
+  <StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </StrictMode>
+);
