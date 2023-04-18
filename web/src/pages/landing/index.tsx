2d24d59d3be063e10e6a9ac423c52ae1ca051974
Zakaria Mansouri <zakaria.mansouri@avimedical.com>
2023-04-19 00:53:00 +0200

    added a "WIP" content in landing page

--- web/src/pages/landing/index.tsx
+++ web/src/pages/landing/index.tsx
@@ -1,9 +1,27 @@
 import { FC } from "react";
+import { Link } from "src/components/link";
+import { Stack } from "src/components/stack";
+import { Text } from "src/components/text";
 
 export default (): ReturnType<FC> => {
   return (
-    <div>
-      <h1>Landing Page</h1>
-    </div>
+    <Stack orientation="vertical">
+      <div className="absolute-center">
+        <Stack orientation="vertical">
+          <Text variant="v1" margin="1">
+            Join a startup in Algeria
+          </Text>
+          <Text variant="v4" margin="1">
+            Source code is publicly available at{" "}
+            <Link
+              to="https://github.com/algeriastartupjobs/algeriastartupjobs.com"
+              variant="v4"
+            >
+              Github
+            </Link>
+          </Text>
+        </Stack>
+      </div>
+    </Stack>
   );
 };
