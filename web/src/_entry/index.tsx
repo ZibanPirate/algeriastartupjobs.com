fde8f43aa00e40261fc444602b723c237b619cfc
Zakaria Mansouri <zakaria.mansouri@avimedical.com>
2023-04-18 18:16:38 +0200

    basic css

--- web/src/_entry/index.tsx
+++ web/src/_entry/index.tsx
@@ -1,3 +1,5 @@
+import "./style.css";
+
 export function App() {
   return <div>JOIN A STARTUP IN ALGERIA</div>;
 }
--- web/src/main.tsx
+++ web/src/main.tsx
@@ -1,6 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "src/_entry/app";
+import { App } from "src/_entry";
 
 createRoot(document.getElementById("root") as HTMLElement).render(
   <StrictMode>
