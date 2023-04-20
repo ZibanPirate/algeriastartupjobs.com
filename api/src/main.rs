f10580171ab52f1b0dc716fa945896b9ec09de40
Zakaria Mansouri <zakaria.mansouri@avimedical.com>
2023-04-20 21:52:52 +0200

    added axum

--- api/src/main.rs
+++ api/src/main.rs
@@ -1,3 +1,14 @@
-fn main() {
-    println!("Hello, world!");
+use axum::{routing::get, Router};
+
+#[tokio::main]
+async fn main() {
+    // build our application with a single route
+    let app = Router::new().route("/", get(|| async { "Hello, World!" }));
+
+    println!("Server started on port 9090");
+    // run it with hyper on localhost:9090
+    axum::Server::bind(&"0.0.0.0:9090".parse().unwrap())
+        .serve(app.into_make_service())
+        .await
+        .unwrap();
 }
