efecd8a65cf591b97952d1954540d3aab691bdb6
Zakaria Mansouri <zakaria.mansouri@avimedical.com>
2023-04-23 14:33:56 +0200

    don't track on dev, and only enable SEO on prod

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -1,12 +1,14 @@
 import { defineConfig } from "vite";
 import vitePluginFaviconsInject from "vite-plugin-favicons-inject";
 import react from "@vitejs/plugin-react-swc";
+import { ViteEjsPlugin } from "vite-plugin-ejs";
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     react(),
     vitePluginFaviconsInject("./src/assets/svg/logo-square.svg"),
+    ViteEjsPlugin(({ mode }) => ({ mode })),
   ],
   publicDir: "dist",
   resolve: {
