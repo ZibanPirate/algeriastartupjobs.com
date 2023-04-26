a3bc103d1fcd0c060649a2311d02a1cbebf24a1d
Zakaria Mansouri <zakaria.mansouri@avimedical.com>
2023-04-26 20:50:24 +0200

    renamed page/index to page/page

--- web/src/pages/index.tsx
+++ web/src/pages/index.tsx
@@ -1,8 +1,8 @@
 import { globImportToLazyNodes } from "src/utils/vite-only/glob-import-to-lazy-nodes";
 
-const componentModules = import.meta.glob("./*/index.tsx", {
+const componentModules = import.meta.glob("./*/page.tsx", {
   import: "Page",
 });
 
 // @TODO-ZM: nice to have static types for LazyPages properties, Vite plugin?
-export const LazyPages = globImportToLazyNodes(componentModules);
+export const LazyPages = globImportToLazyNodes(componentModules, "page");
--- web/src/utils/vite-only/glob-import-to-lazy-nodes.tsx
+++ web/src/utils/vite-only/glob-import-to-lazy-nodes.tsx
@@ -2,13 +2,15 @@ import { ReactNode } from "react";
 import { PreconfiguredLoadable } from "src/components/loadable-factory";
 
 export const globImportToLazyNodes = (
-  modules: Record<string, () => Promise<unknown>>
+  modules: Record<string, () => Promise<unknown>>,
+  index = "index"
 ) => {
   const lazyNodes: Record<string, ReactNode> = {};
 
   Object.keys(modules).forEach((path) => {
     const importer = modules[path];
-    const [, componentName] = path.match(/\/(\S+)\/index\.tsx$/) ?? [];
+    const [, componentName] =
+      path.match(new RegExp(`/(\\S+)\/${index}\\.tsx?$`)) ?? [];
     lazyNodes[componentName] = (
       <PreconfiguredLoadable path={componentName} importer={importer} />
     );
