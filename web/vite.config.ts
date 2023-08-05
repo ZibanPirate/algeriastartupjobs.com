81faef8a84f06b30f4040761ec7ec5d2c75a7f3c
Zakaria Mansouri <zakaria.mansouri@avimedical.com>
2023-08-05 11:30:18 +0200

    optimized svg assets on build time

--- web/vite.config.ts
+++ web/vite.config.ts
@@ -4,6 +4,7 @@ import react from "@vitejs/plugin-react-swc";
 import { ViteEjsPlugin } from "vite-plugin-ejs";
 import { qrcode } from "vite-plugin-qrcode";
 import autoprefixer from "autoprefixer";
+import { ViteImageOptimizer } from "vite-plugin-image-optimizer";
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -12,6 +13,7 @@ export default defineConfig({
     vitePluginFaviconsInject("./src/assets/svg/logo-square.svg"),
     ViteEjsPlugin(({ mode }) => ({ mode })),
     qrcode(),
+    ViteImageOptimizer(),
   ],
   publicDir: "dist",
   resolve: {
