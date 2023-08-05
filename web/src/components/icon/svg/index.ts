6fd9143e5c46cc0f07db50fef176230a4b89fee3
Zakaria Mansouri <zakaria.mansouri@avimedical.com>
2023-08-05 10:56:46 +0200

    added addTag removeTag icons

--- web/src/components/icon/svg/index.ts
+++ web/src/components/icon/svg/index.ts
@@ -4,3 +4,5 @@ export * as back from "./back.svg";
 export * as dropdown from "./dropdown.svg";
 export * as loadingSpinner from "./loading-spinner.svg";
 export * as success from "./success.svg";
+export * as addTag from "./add-tag.svg";
+export * as removeTag from "./remove-tag.svg";
