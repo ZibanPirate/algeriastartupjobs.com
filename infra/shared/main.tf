306477582858ea7ea74c0eb20801046f22a06b80
Zakaria Mansouri <zakman.dev@gmail.com>
2024-03-03 12:42:25 +0100

    force refactoring shared infra

--- infra/shared/main.tf
+++ infra/shared/main.tf
@@ -16,6 +16,35 @@ variable "namecheap_client_ip" {
   sensitive   = true
 }
 
+variable "do_api_key" {
+  description = "The API key for the DigitalOcean account"
+  type        = string
+  sensitive   = true
+}
+
+variable "do_ssh_pub_key" {
+  description = "The SSH key for the DigitalOcean droptlet"
+  type        = string
+  sensitive   = true
+}
+
+locals {
+  root_domain_name        = "algeriastartupjobs.com"
+  is_shared_workspace     = terraform.workspace == "shared"
+  count                   = local.is_shared_workspace ? 1 : 0
+  contact_email_address   = "contact@algeriastartupjobs.com"
+  api_root_domain_name    = "api.algeriastartupjobs.com"
+  assets_root_domain_name = "assets.algeriastartupjobs.com"
+  # @TODO-ZM: make this more generic
+  email_dns_records = [
+    { type : "MX", name : "", value : ["10 mx.zoho.com", "20 mx2.zoho.com", "50 mx3.zoho.com"] },
+    { type : "TXT", name : "", value : ["v=spf1 include:zeptomail.net mx include:zoho.com ~all", "google-site-verification=cSr0V7GyACe-BFr9GaKtVFFw6bz_fTuiJ1cJDraevzY"] },
+    { type : "TXT", name : "dkim._domainkey", value : ["v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDCm72cEJ58s0O+DDFEuYbfofjbNxav37gs4avX784W6s7IOYTdUJcodUCfSUVmb3rPxocVhu5yU3X81BuETG54kg9hgSePx8FANURvkEKbLyyYZZes1g5zhJ1KK7mLkKn5wKnD54WDlrokf6u2TBw9oNU5vlYU1ZHgtxYQ2xmSaQIDAQAB"] },
+    { type : "TXT", name : "29611._domainkey", value : ["k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCaK3ZkrLUeTAss2WebIPPDR1hGuDhczxcD0NqGXgSY7ecYO9PRSQaOQh20+rSEIJGhfhDNfYitFcZYolzp64hanJfiBP3JK7yVRrgT1WDa95E6rwZ7syciGRVyWj4wqFK+ys3JHfNguqXyyylktKKhGzMVILrN2xi3OPRc0UDZ7wIDAQAB"] },
+    { type : "CNAME", name : "mail", value : ["cluster89.zeptomail.com"] },
+  ]
+}
+
 terraform {
   required_providers {
     digitalocean = {
@@ -42,185 +71,156 @@ provider "namecheap" {
   use_sandbox = false
 }
 
-locals {
-  root_domain_name        = "algeriastartupjobs.com"
-  is_shared_workspace     = terraform.workspace == "shared"
-  count                   = local.is_shared_workspace ? 1 : 0
-  contact_email_address   = "contact@algeriastartupjobs.com"
-  api_root_domain_name    = "api.algeriastartupjobs.com"
-  assets_root_domain_name = "assets.algeriastartupjobs.com"
-  # @TODO-ZM: make this more generic
-  email_dns_records = [
-    { type : "MX", name : "", value : ["10 mx.zoho.com", "20 mx2.zoho.com", "50 mx3.zoho.com"] },
-    { type : "TXT", name : "", value : ["v=spf1 include:zeptomail.net mx include:zoho.com ~all", "google-site-verification=cSr0V7GyACe-BFr9GaKtVFFw6bz_fTuiJ1cJDraevzY"] },
-    { type : "TXT", name : "dkim._domainkey", value : ["v=DKIM1; k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQDCm72cEJ58s0O+DDFEuYbfofjbNxav37gs4avX784W6s7IOYTdUJcodUCfSUVmb3rPxocVhu5yU3X81BuETG54kg9hgSePx8FANURvkEKbLyyYZZes1g5zhJ1KK7mLkKn5wKnD54WDlrokf6u2TBw9oNU5vlYU1ZHgtxYQ2xmSaQIDAQAB"] },
-    { type : "TXT", name : "29611._domainkey", value : ["k=rsa; p=MIGfMA0GCSqGSIb3DQEBAQUAA4GNADCBiQKBgQCaK3ZkrLUeTAss2WebIPPDR1hGuDhczxcD0NqGXgSY7ecYO9PRSQaOQh20+rSEIJGhfhDNfYitFcZYolzp64hanJfiBP3JK7yVRrgT1WDa95E6rwZ7syciGRVyWj4wqFK+ys3JHfNguqXyyylktKKhGzMVILrN2xi3OPRc0UDZ7wIDAQAB"] },
-    { type : "CNAME", name : "mail", value : ["cluster89.zeptomail.com"] },
-  ]
-}
+# provider "aws" {
+#   region = "eu-west-1"
+# }
 
-provider "aws" {
-  region = "eu-west-1"
-}
+# # Needed for creating CloudFront distributions
+# provider "aws" {
+#   alias  = "virginia"
+#   region = "us-east-1"
+# }
 
-# Needed for creating CloudFront distributions
-provider "aws" {
-  alias  = "virginia"
-  region = "us-east-1"
-}
+# provider "acme" {
+#   # server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
+#   server_url = "https://acme-v02.api.letsencrypt.org/directory"
+# }
 
-provider "acme" {
-  # server_url = "https://acme-staging-v02.api.letsencrypt.org/directory"
-  server_url = "https://acme-v02.api.letsencrypt.org/directory"
-}
+# # Shared Route53 zone configuration
+# resource "aws_route53_zone" "website" {
+#   count         = local.count
+#   name          = local.root_domain_name
+#   force_destroy = true
+# }
 
-# Shared Route53 zone configuration
-resource "aws_route53_zone" "website" {
-  count         = local.count
-  name          = local.root_domain_name
-  force_destroy = true
-}
-
-# Output the zone ID
-output "route53_zone_id" {
-  value = local.is_shared_workspace ? aws_route53_zone.website[0].id : null
-}
+# # Output the zone ID
+# output "route53_zone_id" {
+#   value = local.is_shared_workspace ? aws_route53_zone.website[0].id : null
+# }
 
 
-resource "aws_acm_certificate" "website" {
-  count                     = local.count
-  domain_name               = local.assets_root_domain_name
-  validation_method         = "DNS"
-  subject_alternative_names = ["*.${local.assets_root_domain_name}"]
-  lifecycle {
-    create_before_destroy = true
-  }
-  provider = aws.virginia
-}
+# resource "aws_acm_certificate" "website" {
+#   count                     = local.count
+#   domain_name               = local.assets_root_domain_name
+#   validation_method         = "DNS"
+#   subject_alternative_names = ["*.${local.assets_root_domain_name}"]
+#   lifecycle {
+#     create_before_destroy = true
+#   }
+#   provider = aws.virginia
+# }
 
-# Output the certificate ARN
-output "certificate_arn" {
-  value = local.is_shared_workspace ? aws_acm_certificate.website[0].arn : null
-}
+# # Output the certificate ARN
+# output "certificate_arn" {
+#   value = local.is_shared_workspace ? aws_acm_certificate.website[0].arn : null
+# }
 
-resource "aws_route53_record" "website" {
-  for_each = {
-    for dvo in aws_acm_certificate.website[0].domain_validation_options : dvo.domain_name => {
-      name   = dvo.resource_record_name
-      record = dvo.resource_record_value
-      type   = dvo.resource_record_type
-    }
-  }
+# resource "aws_route53_record" "website" {
+#   for_each = {
+#     for dvo in aws_acm_certificate.website[0].domain_validation_options : dvo.domain_name => {
+#       name   = dvo.resource_record_name
+#       record = dvo.resource_record_value
+#       type   = dvo.resource_record_type
+#     }
+#   }
 
-  allow_overwrite = true
-  name            = each.value.name
-  records         = [each.value.record]
-  ttl             = 60
-  type            = each.value.type
-  zone_id         = aws_route53_zone.website[0].id
-}
+#   allow_overwrite = true
+#   name            = each.value.name
+#   records         = [each.value.record]
+#   ttl             = 60
+#   type            = each.value.type
+#   zone_id         = aws_route53_zone.website[0].id
+# }
 
-resource "aws_acm_certificate_validation" "website" {
-  certificate_arn         = aws_acm_certificate.website[0].arn
-  validation_record_fqdns = [for record in aws_route53_record.website : record.fqdn]
-  provider                = aws.virginia
-}
+# resource "aws_acm_certificate_validation" "website" {
+#   certificate_arn         = aws_acm_certificate.website[0].arn
+#   validation_record_fqdns = [for record in aws_route53_record.website : record.fqdn]
+#   provider                = aws.virginia
+# }
 
-resource "aws_route53_record" "email" {
-  for_each = {
-    for record in local.email_dns_records : index(local.email_dns_records, record) => {
-      name    = record.name
-      records = record.value
-      type    = record.type
-    }
-  }
+# resource "aws_route53_record" "email" {
+#   for_each = {
+#     for record in local.email_dns_records : index(local.email_dns_records, record) => {
+#       name    = record.name
+#       records = record.value
+#       type    = record.type
+#     }
+#   }
 
-  allow_overwrite = true
-  ttl             = 60
-  name            = each.value.name
-  type            = each.value.type
-  zone_id         = aws_route53_zone.website[0].id
-  records         = each.value.records
-}
+#   allow_overwrite = true
+#   ttl             = 60
+#   name            = each.value.name
+#   type            = each.value.type
+#   zone_id         = aws_route53_zone.website[0].id
+#   records         = each.value.records
+# }
 
-resource "aws_route53_record" "github" {
-  allow_overwrite = true
-  ttl             = 60
-  name            = "_github-challenge-algeriastartupjobs-org.algeriastartupjobs.com"
-  type            = "TXT"
-  zone_id         = aws_route53_zone.website[0].id
-  records         = ["029060ef0f"]
-}
+# resource "aws_route53_record" "github" {
+#   allow_overwrite = true
+#   ttl             = 60
+#   name            = "_github-challenge-algeriastartupjobs-org.algeriastartupjobs.com"
+#   type            = "TXT"
+#   zone_id         = aws_route53_zone.website[0].id
+#   records         = ["029060ef0f"]
+# }
 
-variable "do_api_key" {
-  description = "The API key for the DigitalOcean account"
-  type        = string
-  sensitive   = true
-}
+# provider "digitalocean" {
+#   token = var.do_api_key
+# }
 
-variable "do_ssh_pub_key" {
-  description = "The SSH key for the DigitalOcean droptlet"
-  type        = string
-  sensitive   = true
-}
+# resource "digitalocean_ssh_key" "api" {
+#   count      = local.count
+#   name       = "Algeria Startup Jobs Terraform Key"
+#   public_key = var.do_ssh_pub_key
+# }
 
-provider "digitalocean" {
-  token = var.do_api_key
-}
+# output "digitalocean_ssh_key_fingerprint" {
+#   value = local.is_shared_workspace ? digitalocean_ssh_key.api[0].fingerprint : null
+# }
 
-resource "digitalocean_ssh_key" "api" {
-  count      = local.count
-  name       = "Algeria Startup Jobs Terraform Key"
-  public_key = var.do_ssh_pub_key
-}
+# resource "digitalocean_project" "api" {
+#   count = local.count
+#   name  = "Algeria Startup Jobs"
+# }
 
-output "digitalocean_ssh_key_fingerprint" {
-  value = local.is_shared_workspace ? digitalocean_ssh_key.api[0].fingerprint : null
-}
+# output "digitalocean_project_id" {
+#   value = local.is_shared_workspace ? digitalocean_project.api[0].id : null
+# }
 
-resource "digitalocean_project" "api" {
-  count = local.count
-  name  = "Algeria Startup Jobs"
-}
+# resource "tls_private_key" "api" {
+#   count     = local.count
+#   algorithm = "RSA"
+# }
 
-output "digitalocean_project_id" {
-  value = local.is_shared_workspace ? digitalocean_project.api[0].id : null
-}
+# resource "acme_registration" "api" {
+#   count           = local.count
+#   account_key_pem = tls_private_key.api[0].private_key_pem
+#   email_address   = local.contact_email_address
+# }
 
-resource "tls_private_key" "api" {
-  count     = local.count
-  algorithm = "RSA"
-}
+# resource "acme_certificate" "api" {
+#   count                     = local.count
+#   account_key_pem           = acme_registration.api[0].account_key_pem
+#   common_name               = local.root_domain_name
+#   subject_alternative_names = ["*.${local.root_domain_name}", "*.${local.api_root_domain_name}"]
 
-resource "acme_registration" "api" {
-  count           = local.count
-  account_key_pem = tls_private_key.api[0].private_key_pem
-  email_address   = local.contact_email_address
-}
+#   dns_challenge {
+#     provider = "route53"
 
-resource "acme_certificate" "api" {
-  count                     = local.count
-  account_key_pem           = acme_registration.api[0].account_key_pem
-  common_name               = local.root_domain_name
-  subject_alternative_names = ["*.${local.root_domain_name}", "*.${local.api_root_domain_name}"]
+#     config = {
+#       AWS_HOSTED_ZONE_ID = aws_route53_zone.website[0].id
+#     }
+#   }
 
-  dns_challenge {
-    provider = "route53"
+#   depends_on = [acme_registration.api[0]]
+# }
 
-    config = {
-      AWS_HOSTED_ZONE_ID = aws_route53_zone.website[0].id
-    }
-  }
+# output "acme_certificate_api_certificate_pem" {
+#   value     = local.is_shared_workspace ? acme_certificate.api[0].certificate_pem : null
+#   sensitive = true
+# }
 
-  depends_on = [acme_registration.api[0]]
-}
-
-output "acme_certificate_api_certificate_pem" {
-  value     = local.is_shared_workspace ? acme_certificate.api[0].certificate_pem : null
-  sensitive = true
-}
-
-output "acme_certificate_api_private_key_pem" {
-  value     = local.is_shared_workspace ? acme_certificate.api[0].private_key_pem : null
-  sensitive = true
-}
+# output "acme_certificate_api_private_key_pem" {
+#   value     = local.is_shared_workspace ? acme_certificate.api[0].private_key_pem : null
+#   sensitive = true
+# }
