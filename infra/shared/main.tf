cb12551a5452e320960a57f9796bc337a08da094
Zakaria Mansouri <zakman.dev@gmail.com>
2024-03-03 12:58:37 +0100

    Refactor AWS Route53 record resource in main.tf

--- infra/shared/main.tf
+++ infra/shared/main.tf
@@ -117,22 +117,22 @@ output "certificate_arn" {
   value = local.is_shared_workspace ? aws_acm_certificate.website[0].arn : null
 }
 
-# resource "aws_route53_record" "website" {
-#   for_each = {
-#     for dvo in aws_acm_certificate.website[0].domain_validation_options : dvo.domain_name => {
-#       name   = dvo.resource_record_name
-#       record = dvo.resource_record_value
-#       type   = dvo.resource_record_type
-#     }
-#   }
+resource "aws_route53_record" "website" {
+  for_each = {
+    for dvo in aws_acm_certificate.website[0].domain_validation_options : dvo.domain_name => {
+      name   = dvo.resource_record_name
+      record = dvo.resource_record_value
+      type   = dvo.resource_record_type
+    }
+  }
 
-#   allow_overwrite = true
-#   name            = each.value.name
-#   records         = [each.value.record]
-#   ttl             = 60
-#   type            = each.value.type
-#   zone_id         = aws_route53_zone.website[0].id
-# }
+  allow_overwrite = true
+  name            = each.value.name
+  records         = [each.value.record]
+  ttl             = 60
+  type            = each.value.type
+  zone_id         = aws_route53_zone.website[0].id
+}
 
 # resource "aws_acm_certificate_validation" "website" {
 #   certificate_arn         = aws_acm_certificate.website[0].arn
